3b3d60259e86ea4913c7279f9c4a151a3b3bbc6a
DaneeXIII <szarkadani0302@gmail.com>
2021-04-20 16:25:21 +0200

    Fejhallgató rendelés leadva, 2. activity.

--- app/src/main/java/com/example/projekt2/Feldolgozo.java
+++ app/src/main/java/com/example/projekt2/Feldolgozo.java
@@ -3,6 +3,7 @@ package com.example.projekt2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Feldolgozo extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class Feldolgozo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feldolgozo);
+
+        TextView sikeres = findViewById(R.id.sikeres);
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            sikeres.setText(("Köszönjük a rendelést, " + extras.getString("nev")) + "! A fizetendő összeg 30.000 Ft lesz. ");
+        }
     }
 }
